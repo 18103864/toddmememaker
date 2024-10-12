2b7401ac3b590d6bdfbe939c10d5346538ff321a
Carl <18103864@usc.edu.ph>
2024-10-12 08:20:30 +0800

    added more assets

--- mememaker-assets/scripts/script.js
+++ mememaker-assets/scripts/script.js
@@ -14,7 +14,7 @@ var current_bg = undefined;
 var current_bottom = undefined;
 var current_top = undefined;
 
-var selected_body = 0;
+var selected_body = 1;
 var selected_head = 0;
 var selected_eyes = 0;
 var selected_mouth = 0;
@@ -145,8 +145,20 @@ function selectParts(type, idx) {
     setSelectedIdx(type, idx);
 }
 
-AddParts(bodyList, 'body',0, 'mememaker-assets/img/icons/body/1.png', 'mememaker-assets/img/body/1.png', true);
-AddParts(bodyList, 'body',1, 'mememaker-assets/img/icons/body/2.png', 'mememaker-assets/img/body/2.png');
+AddParts(bodyList, 'body',1, 'mememaker-assets/img/icons/body/1.png', 'mememaker-assets/img/body/1.png', true);
+AddParts(bodyList, 'body',2, 'mememaker-assets/img/icons/body/2.png', 'mememaker-assets/img/body/2.png');
+AddParts(bodyList, 'body',3, 'mememaker-assets/img/icons/body/3.png', 'mememaker-assets/img/body/3.png');
+AddParts(bodyList, 'body',4, 'mememaker-assets/img/icons/body/4.png', 'mememaker-assets/img/body/4.png');
+AddParts(bodyList, 'body',5, 'mememaker-assets/img/icons/body/5.png', 'mememaker-assets/img/body/5.png');
+AddParts(bodyList, 'body',6, 'mememaker-assets/img/icons/body/6.png', 'mememaker-assets/img/body/6.png');
+AddParts(bodyList, 'body',7, 'mememaker-assets/img/icons/body/7.png', 'mememaker-assets/img/body/7.png');
+AddParts(bodyList, 'body',8, 'mememaker-assets/img/icons/body/8.png', 'mememaker-assets/img/body/8.png');
+AddParts(bodyList, 'body',9, 'mememaker-assets/img/icons/body/9.png', 'mememaker-assets/img/body/9.png');
+AddParts(bodyList, 'body',10, 'mememaker-assets/img/icons/body/10.png', 'mememaker-assets/img/body/10.png');
+AddParts(bodyList, 'body',11, 'mememaker-assets/img/icons/body/11.png', 'mememaker-assets/img/body/11.png');
+AddParts(bodyList, 'body',12, 'mememaker-assets/img/icons/body/12.png', 'mememaker-assets/img/body/12.png');
+AddParts(bodyList, 'body',13, 'mememaker-assets/img/icons/body/13.png', 'mememaker-assets/img/body/13.png');
+AddParts(bodyList, 'body',14, 'mememaker-assets/img/icons/body/14.png', 'mememaker-assets/img/body/14.png');
 
 AddParts(headList, 'head',0, 'mememaker-assets/img/none.png', undefined, true);
 AddParts(headList, 'head',1, 'mememaker-assets/img/icons/head/1.png', 'mememaker-assets/img/head/1.png');
@@ -160,15 +172,48 @@ AddParts(headList, 'head',8, 'mememaker-assets/img/icons/head/8.png', 'mememaker
 AddParts(headList, 'head',9, 'mememaker-assets/img/icons/head/9.png', 'mememaker-assets/img/head/9.png');
 AddParts(headList, 'head',10, 'mememaker-assets/img/icons/head/10.png', 'mememaker-assets/img/head/10.png');
 AddParts(headList, 'head',11, 'mememaker-assets/img/icons/head/11.png', 'mememaker-assets/img/head/11.png');
-// AddParts(headList, 'head',12, 'mememaker-assets/img/icons/head/12.png', 'mememaker-assets/img/head/12.png');
+AddParts(headList, 'head',13, 'mememaker-assets/img/icons/head/13.png', 'mememaker-assets/img/head/13.png');
+AddParts(headList, 'head',14, 'mememaker-assets/img/icons/head/14.png', 'mememaker-assets/img/head/14.png');
+AddParts(headList, 'head',15, 'mememaker-assets/img/icons/head/15.png', 'mememaker-assets/img/head/15.png');
+AddParts(headList, 'head',16, 'mememaker-assets/img/icons/head/16.png', 'mememaker-assets/img/head/16.png');
+AddParts(headList, 'head',17, 'mememaker-assets/img/icons/head/17.png', 'mememaker-assets/img/head/17.png');
+AddParts(headList, 'head',18, 'mememaker-assets/img/icons/head/18.png', 'mememaker-assets/img/head/18.png');
+AddParts(headList, 'head',19, 'mememaker-assets/img/icons/head/19.png', 'mememaker-assets/img/head/19.png');
+AddParts(headList, 'head',20, 'mememaker-assets/img/icons/head/20.png', 'mememaker-assets/img/head/20.png');
+AddParts(headList, 'head',21, 'mememaker-assets/img/icons/head/21.png', 'mememaker-assets/img/head/21.png');
+AddParts(headList, 'head',22, 'mememaker-assets/img/icons/head/22.png', 'mememaker-assets/img/head/22.png');
+AddParts(headList, 'head',23, 'mememaker-assets/img/icons/head/23.png', 'mememaker-assets/img/head/23.png');
+AddParts(headList, 'head',24, 'mememaker-assets/img/icons/head/24.png', 'mememaker-assets/img/head/24.png');
+AddParts(headList, 'head',25, 'mememaker-assets/img/icons/head/25.png', 'mememaker-assets/img/head/25.png');
+AddParts(headList, 'head',26, 'mememaker-assets/img/icons/head/26.png', 'mememaker-assets/img/head/26.png');
+AddParts(headList, 'head',27, 'mememaker-assets/img/icons/head/27.png', 'mememaker-assets/img/head/27.png');
+AddParts(headList, 'head',28, 'mememaker-assets/img/icons/head/28.png', 'mememaker-assets/img/head/28.png');
+AddParts(headList, 'head',29, 'mememaker-assets/img/icons/head/29.png', 'mememaker-assets/img/head/29.png');
+AddParts(headList, 'head',30, 'mememaker-assets/img/icons/head/30.png', 'mememaker-assets/img/head/30.png');
+// AddParts(headList, 'head',31, 'mememaker-assets/img/icons/head/31.png', 'mememaker-assets/img/head/31.png');
+// AddParts(headList, 'head',32, 'mememaker-assets/img/icons/head/32.png', 'mememaker-assets/img/head/32.png');
+// AddParts(headList, 'head',33, 'mememaker-assets/img/icons/head/33.png', 'mememaker-assets/img/head/33.png');
+// AddParts(headList, 'head',34, 'mememaker-assets/img/icons/head/34.png', 'mememaker-assets/img/head/34.png');
+// AddParts(headList, 'head',35, 'mememaker-assets/img/icons/head/35.png', 'mememaker-assets/img/head/35.png');
+// AddParts(headList, 'head',36, 'mememaker-assets/img/icons/head/36.png', 'mememaker-assets/img/head/36.png');
+// AddParts(headList, 'head',37, 'mememaker-assets/img/icons/head/37.png', 'mememaker-assets/img/head/37.png');
+// AddParts(headList, 'head',38, 'mememaker-assets/img/icons/head/38.png', 'mememaker-assets/img/head/38.png');
+// AddParts(headList, 'head',39, 'mememaker-assets/img/icons/head/39.png', 'mememaker-assets/img/head/39.png');
+// AddParts(headList, 'head',40, 'mememaker-assets/img/icons/head/40.png', 'mememaker-assets/img/head/40.png');
+// AddParts(headList, 'head',41, 'mememaker-assets/img/icons/head/41.png', 'mememaker-assets/img/head/41.png');
+// AddParts(headList, 'head',42, 'mememaker-assets/img/icons/head/42.png', 'mememaker-assets/img/head/42.png');
+// AddParts(headList, 'head',43, 'mememaker-assets/img/icons/head/43.png', 'mememaker-assets/img/head/43.png');
+// AddParts(headList, 'head',44, 'mememaker-assets/img/icons/head/44.png', 'mememaker-assets/img/head/44.png');
+// AddParts(headList, 'head',45, 'mememaker-assets/img/icons/head/45.png', 'mememaker-assets/img/head/45.png');
+
 
 AddParts(eyesList, 'eyes',0, 'mememaker-assets/img/none.png', undefined, true);
 AddParts(eyesList, 'eyes',1, 'mememaker-assets/img/icons/eyes/1.png', 'mememaker-assets/img/eyes/1.png');
 AddParts(eyesList, 'eyes',2, 'mememaker-assets/img/icons/eyes/2.png', 'mememaker-assets/img/eyes/2.png');
 AddParts(eyesList, 'eyes',3, 'mememaker-assets/img/icons/eyes/3.png', 'mememaker-assets/img/eyes/3.png');
 AddParts(eyesList, 'eyes',4, 'mememaker-assets/img/icons/eyes/4.png', 'mememaker-assets/img/eyes/4.png');
-// AddParts(eyesList, 'eyes',5, 'mememaker-assets/img/icons/eyes/5.png', 'mememaker-assets/img/eyes/5.png');
-// AddParts(eyesList, 'eyes',6, 'mememaker-assets/img/icons/eyes/6.png', 'mememaker-assets/img/eyes/6.png');
+AddParts(eyesList, 'eyes',5, 'mememaker-assets/img/icons/eyes/5.png', 'mememaker-assets/img/eyes/5.png');
+AddParts(eyesList, 'eyes',6, 'mememaker-assets/img/icons/eyes/6.png', 'mememaker-assets/img/eyes/6.png');
 // AddParts(eyesList, 'eyes',7, 'mememaker-assets/img/icons/eyes/7.png', 'mememaker-assets/img/eyes/7.png');
 // AddParts(eyesList, 'eyes',8, 'mememaker-assets/img/icons/eyes/8.png', 'mememaker-assets/img/eyes/8.png');
 
@@ -190,10 +235,22 @@ AddParts(topList, 'top',5, 'mememaker-assets/img/icons/top/5.png', 'mememaker-as
 AddParts(topList, 'top',6, 'mememaker-assets/img/icons/top/6.png', 'mememaker-assets/img/top/6.png');
 AddParts(topList, 'top',7, 'mememaker-assets/img/icons/top/7.png', 'mememaker-assets/img/top/7.png');
 AddParts(topList, 'top',8, 'mememaker-assets/img/icons/top/8.png', 'mememaker-assets/img/top/8.png');
-// AddParts(topList, 'top',9, 'mememaker-assets/img/icons/top/9.png', 'mememaker-assets/img/top/9.png');
-// AddParts(topList, 'top',10, 'mememaker-assets/img/icons/top/10.png', 'mememaker-assets/img/top/10.png');
-// AddParts(topList, 'top',11, 'mememaker-assets/img/icons/top/11.png', 'mememaker-assets/img/top/11.png');
-// AddParts(topList, 'top',12, 'mememaker-assets/img/icons/top/012.png', 'mememaker-assets/img/top/012.png');
+AddParts(topList, 'top',9, 'mememaker-assets/img/icons/top/9.png', 'mememaker-assets/img/top/9.png');
+AddParts(topList, 'top',10, 'mememaker-assets/img/icons/top/10.png', 'mememaker-assets/img/top/10.png');
+AddParts(topList, 'top',11, 'mememaker-assets/img/icons/top/11.png', 'mememaker-assets/img/top/11.png');
+AddParts(topList, 'top',12, 'mememaker-assets/img/icons/top/12.png', 'mememaker-assets/img/top/12.png');
+AddParts(topList, 'top',13, 'mememaker-assets/img/icons/top/13.png', 'mememaker-assets/img/top/13.png');
+AddParts(topList, 'top',14, 'mememaker-assets/img/icons/top/14.png', 'mememaker-assets/img/top/14.png');
+AddParts(topList, 'top',15, 'mememaker-assets/img/icons/top/15.png', 'mememaker-assets/img/top/15.png');
+AddParts(topList, 'top',16, 'mememaker-assets/img/icons/top/16.png', 'mememaker-assets/img/top/16.png');
+AddParts(topList, 'top',17, 'mememaker-assets/img/icons/top/17.png', 'mememaker-assets/img/top/17.png');
+AddParts(topList, 'top',18, 'mememaker-assets/img/icons/top/18.png', 'mememaker-assets/img/top/18.png');
+AddParts(topList, 'top',19, 'mememaker-assets/img/icons/top/19.png', 'mememaker-assets/img/top/19.png');
+AddParts(topList, 'top',20, 'mememaker-assets/img/icons/top/20.png', 'mememaker-assets/img/top/20.png');
+AddParts(topList, 'top',21, 'mememaker-assets/img/icons/top/21.png', 'mememaker-assets/img/top/21.png');
+AddParts(topList, 'top',22, 'mememaker-assets/img/icons/top/22.png', 'mememaker-assets/img/top/22.png');
+AddParts(topList, 'top',23, 'mememaker-assets/img/icons/top/23.png', 'mememaker-assets/img/top/23.png');
+
 
 AddParts(bottomList, 'bottom', 0, 'mememaker-assets/img/none.png', undefined, true);
 AddParts(bottomList, 'bottom', 1, 'mememaker-assets/img/icons/bottom/1.png', 'mememaker-assets/img/bottom/1.png');
@@ -204,9 +261,14 @@ AddParts(bottomList, 'bottom', 5, 'mememaker-assets/img/icons/bottom/5.png', 'me
 AddParts(bottomList, 'bottom', 6, 'mememaker-assets/img/icons/bottom/6.png', 'mememaker-assets/img/bottom/6.png');
 AddParts(bottomList, 'bottom', 7, 'mememaker-assets/img/icons/bottom/7.png', 'mememaker-assets/img/bottom/7.png');
 AddParts(bottomList, 'bottom', 8, 'mememaker-assets/img/icons/bottom/8.png', 'mememaker-assets/img/bottom/8.png');
-// AddParts(bottomList, 'bottom', 9, 'mememaker-assets/img/icons/bottom/009.png', 'mememaker-assets/img/bottom/009.png');
-// AddParts(bottomList, 'bottom', 10, 'mememaker-assets/img/icons/bottom/010.png', 'mememaker-assets/img/bottom/010.png');
-// AddParts(bottomList, 'bottom', 11, 'mememaker-assets/img/icons/bottom/11.png', 'mememaker-assets/img/bottom/11.png');
+AddParts(bottomList, 'bottom', 9, 'mememaker-assets/img/icons/bottom/9.png', 'mememaker-assets/img/bottom/9.png');
+AddParts(bottomList, 'bottom', 10, 'mememaker-assets/img/icons/bottom/10.png', 'mememaker-assets/img/bottom/10.png');
+AddParts(bottomList, 'bottom', 11, 'mememaker-assets/img/icons/bottom/11.png', 'mememaker-assets/img/bottom/11.png');
+AddParts(bottomList, 'bottom', 12, 'mememaker-assets/img/icons/bottom/12.png', 'mememaker-assets/img/bottom/12.png');
+AddParts(bottomList, 'bottom', 13, 'mememaker-assets/img/icons/bottom/13.png', 'mememaker-assets/img/bottom/13.png');
+AddParts(bottomList, 'bottom', 14, 'mememaker-assets/img/icons/bottom/14.png', 'mememaker-assets/img/bottom/14.png');
+AddParts(bottomList, 'bottom', 15, 'mememaker-assets/img/icons/bottom/15.png', 'mememaker-assets/img/bottom/15.png');
+AddParts(bottomList, 'bottom', 16, 'mememaker-assets/img/icons/bottom/16.png', 'mememaker-assets/img/bottom/16.png');
 
 AddParts(bgList, 'bg', 1, 'mememaker-assets/img/icons/background/1.png', 'mememaker-assets/img/background/1.png', true);
 // AddParts(bgList, 'bg', 2, 'mememaker-assets/img/icons/background/2.png', 'mememaker-assets/img/background/2.png');
@@ -271,19 +333,19 @@ resetBtn.onclick = () => {
     selectParts('bg', 1);
     selectParts('bottom', 0);
     selectParts('top', 0);
-    selectParts('body', 0);
+    selectParts('body', 1);
 }
 
 const randomBtn = document.getElementById('random-btn');
 
 randomBtn.onclick = () => {
-    const randHead = randRange(0, 12);
-    const randEyes = randRange(0, 5);
+    const randHead = randRange(0, 31);
+    const randEyes = randRange(0, 7);
     // const randMouth = randRange(0, 2);
      const randBg = randRange(0, 2);
     const randbottom = randRange(0, 9);
-    const randtop = randRange(0, 9);
-    const randbody = randRange(0, 2);
+    const randtop = randRange(0, 24);
+    const randbody = randRange(0, 15);
 
     selectParts('head', randHead);
     selectParts('eyes', randEyes);
